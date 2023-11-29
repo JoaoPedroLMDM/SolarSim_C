176d9f63857b6c64bdb9d4eae33e5cf1a709f078
joaopedrolobomedeiros <joaopedrolobomedeiros@gmail.com>
2023-11-29 16:48:51 -0300

    inicial

---
+++ src/main/java/com/mycompany/solarsim/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.solarsim;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Cliente {
+    String nome;
+    String cpf;
+    String endereco;
+    String email;
+    String senha;
+    String telefone;
+}
