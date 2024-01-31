f3466da3d5446e006e7dee72e1e49d29ee949a55
JoaoPedroLMDM <151780872+JoaoPedroLMDM@users.noreply.github.com>
2024-01-31 15:35:38 -0300

    Update Cliente.java

--- src/main/java/com/mycompany/solarsim/Cliente.java
+++ src/main/java/com/mycompany/solarsim/Cliente.java
@@ -8,11 +8,31 @@ package com.mycompany.solarsim;
  *
  * @author Aluno
  */
-public class Cliente {
-    String nome;
+public class Cliente extends Pessoa {
     String cpf;
-    String endereco;
-    String email;
     String senha;
-    String telefone;
+
+    // Construtor da classe Cliente
+    public Cliente(String nome, String endereco, String email, String telefone, String cpf, String senha) {
+        super(nome, endereco, email, telefone);
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    // Métodos de acesso para a classe Cliente
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
