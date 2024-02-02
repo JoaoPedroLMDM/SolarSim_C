e28aa5fc2e0fea3522cfc1be51a67c08c765f668
JoaoPedroLMDM <151780872+JoaoPedroLMDM@users.noreply.github.com>
2024-02-02 11:12:28 -0300

    Update SolarSim.java

--- src/main/java/com/mycompany/solarsim/SolarSim.java
+++ src/main/java/com/mycompany/solarsim/SolarSim.java
@@ -11,6 +11,8 @@ package com.mycompany.solarsim;
 public class SolarSim {
 
     public static void main(String[] args) {
+
+        public void menu()
         System.out.println("Hello World!");
     }
 }
