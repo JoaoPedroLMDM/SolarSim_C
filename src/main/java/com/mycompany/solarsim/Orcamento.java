6c8713e172edc700e403efc177e7d977756e3b46
JoaoPedroLMDM <151780872+JoaoPedroLMDM@users.noreply.github.com>
2024-02-02 09:42:53 -0300

    Update Orcamento.java

--- src/main/java/com/mycompany/solarsim/Orcamento.java
+++ src/main/java/com/mycompany/solarsim/Orcamento.java
@@ -4,12 +4,42 @@
  */
 package com.mycompany.solarsim;
 
-/**
- *
- * @author Aluno
- */
 public class Orcamento {
-    Cliente cliente;
-    String tetodegastos;
-    String custo;
+    private Cliente cliente;
+    private String tetodegastos;
+    private String custo;
+
+    // Método construtor
+    public Orcamento(Cliente cliente, String tetodegastos, String custo) {
+        this.cliente = cliente;
+        this.tetodegastos = tetodegastos;
+        this.custo = custo;
+    }
+
+    // Métodos de acesso para o cliente
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    // Métodos de acesso para tetodegastos
+    public String getTetodegastos() {
+        return tetodegastos;
+    }
+
+    public void setTetodegastos(String tetodegastos) {
+        this.tetodegastos = tetodegastos;
+    }
+
+    // Métodos de acesso para custo
+    public String getCusto() {
+        return custo;
+    }
+
+    public void setCusto(String custo) {
+        this.custo = custo;
+    }
 }
